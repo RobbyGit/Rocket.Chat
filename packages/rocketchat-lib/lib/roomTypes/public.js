83a90cc80caa1bb3a3525b4fbfd5273d00725725
Diego Sampaio <chinello@gmail.com>
2018-03-01 09:06:31 -0300

    Fix lint

--- packages/rocketchat-lib/lib/roomTypes/public.js
+++ packages/rocketchat-lib/lib/roomTypes/public.js
@@ -1,5 +1,5 @@
 /* globals openRoom */
-import {RoomTypeConfig, RoomTypeRouteConfig, UiTextContext} from '../RoomTypeConfig';
+import { RoomTypeConfig, RoomTypeRouteConfig, RoomSettingsEnum, UiTextContext } from '../RoomTypeConfig';
 
 export class PublicRoomRoute extends RoomTypeRouteConfig {
 	constructor() {
@@ -61,10 +61,6 @@ export class PublicRoomType extends RoomTypeConfig {
 
 	canAddUser(room) {
 		return RocketChat.authz.hasAtLeastOnePermission(['add-user-to-any-c-room', 'add-user-to-joined-room'], room._id);
-	}
-
-	allowRoomSettingChange() {
-		return true;
 	}
 
 	enableMembersListProfile() {
